aa4f5b86dc581f4e294a53dac93b47140ba1bac1
KennyFive05 <kennyfive05@gmail.com>
2019-02-21 10:15:18 +0800

    First

---
+++ src/app/atribute/atribute.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atribute',
+  templateUrl: './atribute.component.html',
+  styleUrls: ['./atribute.component.css']
+})
+export class AtributeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
